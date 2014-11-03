4dd1e168231ed5e5e5e64d1bf0a299ff2372c952
sleepingkingstudios <merlin@sleepingkingstudios.com>
2014-11-03 15:38:05 -0500

    Refactor BlogPost to inherit from Feature.
    Current spec status: 906 examples, 0 failures in 5.9 seconds.

--- app/models/features/blog_post.rb
+++ app/models/features/blog_post.rb
@@ -2,28 +2,7 @@
 
 require 'mongoid/sleeping_king_studios/sluggable'
 
-class BlogPost
-  include Mongoid::Document
-  include Mongoid::SleepingKingStudios::Sluggable
-
-  ### Class Methods ###
-
-  class << self
-    def default_content_type
-      :text
-    end # method default_content_type
-
-    def reserved_slugs
-      Feature.reserved_slugs
-    end # class method reserved_slugs
-  end # class << self
-
-  ### Attributes ###
-  field :title, :type => String
-
-  ### Concerns ###
-  slugify :title, :lockable => true
-
+class BlogPost < Feature
   ### Relations ###
   belongs_to :blog, :inverse_of => :posts
 
@@ -32,8 +11,5 @@ class BlogPost
   ### Validations ###
   validates :blog,    :presence => true
   validates :content, :presence => true
-  validates :title,   :presence => true
-  validates :slug,
-    :exclusion  => { :in => ->(document) { document.class.reserved_slugs } },
-    :uniqueness => { :scope => :blog }
+  validates :slug,    :uniqueness => { :scope => :blog }
 end # class
