f5c8f9ae67bd1276ae8cc871194759fdbf73e17c
sleepingkingstudios <merlin@sleepingkingstudios.com>
2014-11-04 10:09:04 -0500

    Implement BlogPosts#create.
    Current spec status: 1012 examples, 0 failures in 6.99 seconds.

--- app/controllers/admin/features/blog_posts_controller.rb
+++ app/controllers/admin/features/blog_posts_controller.rb
@@ -1,9 +1,18 @@
 # app/controllers/admin/features/blog_posts_controller.rb
 
 class Admin::Features::BlogPostsController < Admin::FeaturesController
-  before_action :lookup_blog, :only => %i(index new)
+  # before_action :lookup_blog, :only => %i(index new create)
 
   private
+
+  def initialize_delegate
+    # Insert the #lookup_blog callback in the appropriate point in the callbacks.
+    lookup_blog
+
+    super
+
+    @delegate.blog = @blog
+  end # method initialize_delegate
 
   def lookup_blog
     scope = @current_directory.nil? ? Blog.where(:directory_id => nil) : @current_directory.blogs
--- spec/controllers/admin/features/blog_posts_controller_spec.rb
+++ spec/controllers/admin/features/blog_posts_controller_spec.rb
@@ -133,4 +133,106 @@ RSpec.describe Admin::Features::BlogPostsController do
       end # describe
     end # describe
   end # describe
+
+  describe '#create' do
+    let(:attributes) { {} }
+
+    expect_behavior 'requires authentication'
+
+    expect_behavior 'requires authentication for root directory'
+
+    def perform_action
+      post :create, params
+    end # method perform_action
+
+    before(:each) { sign_in :user, user }
+
+    let(:params) { { :blog => blog.slug, :directories => path, :post => attributes } }
+
+    describe 'with an empty path', :path => :empty do
+      describe 'with an invalid blog' do
+        expect_behavior 'redirects to the last found directory dashboard'
+      end # describe
+
+      describe 'with a valid blog' do
+        let(:blog) { create(:blog, :directory => directories.last) }
+
+        describe 'with invalid params' do
+          let(:attributes) { super().merge :title => nil }
+
+          expect_behavior 'renders template', :new
+
+          expect_behavior 'assigns new resource', BlogPost
+
+          expect_behavior 'assigns directories'
+
+          it 'does not create a blog post' do
+            expect { perform_action }.not_to change(blog.posts, :count)
+          end # it
+        end # describe
+
+        describe 'with valid params' do
+          let(:attributes) { attributes_for(:blog_post).merge :content => attributes_for(:text_content).merge(:_type => 'text_content') }
+
+          it 'redirects to the post' do
+            perform_action
+
+            expect(response.status).to be == 302
+            expect(response).to redirect_to(blog_post_path(assigns :resource))
+
+            expect(request.flash[:success]).not_to be_blank
+          end # it
+
+          it 'creates a new blog post' do
+            expect { perform_action }.to change(blog.posts, :count).by(1)
+          end # it
+        end # describe
+      end # describe
+    end # describe
+
+    describe 'with an invalid path', :path => :invalid_directory do
+      expect_behavior 'redirects to the last found directory dashboard'
+    end # describe
+
+    describe 'with a valid path', :path => :valid_directory do
+      describe 'with an invalid blog' do
+        expect_behavior 'redirects to the last found directory dashboard'
+      end # describe
+
+      describe 'with a valid blog' do
+        let(:blog) { create(:blog, :directory => directories.last) }
+
+        describe 'with invalid params' do
+          let(:attributes) { super().merge :title => nil }
+
+          expect_behavior 'renders template', :new
+
+          expect_behavior 'assigns new resource', BlogPost
+
+          expect_behavior 'assigns directories'
+
+          it 'does not create a blog post' do
+            expect { perform_action }.not_to change(blog.posts, :count)
+          end # it
+        end # describe
+
+        describe 'with valid params' do
+          let(:attributes) { attributes_for(:blog_post).merge :content => attributes_for(:text_content).merge(:_type => 'text_content') }
+
+          it 'redirects to the post' do
+            perform_action
+
+            expect(response.status).to be == 302
+            expect(response).to redirect_to(blog_post_path(assigns :resource))
+
+            expect(request.flash[:success]).not_to be_blank
+          end # it
+
+          it 'creates a new blog post' do
+            expect { perform_action }.to change(blog.posts, :count).by(1)
+          end # it
+        end # describe
+      end # describe
+    end # describe
+  end # describe
 end # describe
